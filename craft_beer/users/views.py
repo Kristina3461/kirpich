7146a596f6157d497e6a7791d9dfb062f1da2ccd
Kristina3461 <117187975+Kristina3461@users.noreply.github.com>
2023-10-24 16:46:42 +0700

    Add files via upload

--- craft_beer/users/views.py
+++ craft_beer/users/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
 from .models import CustomUser
 from .serializers import CustomUserSerializer
 
@@ -6,6 +8,8 @@ from .serializers import CustomUserSerializer
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+    permission_classes = (IsAuthenticated,)
+
 
 
 
